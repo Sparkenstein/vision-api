c39787d0541b72fc7f77c3cc8b3b99f142f90a9e
Sparkenstein <zetabytes.pp@gmail.com>
2020-02-12 13:12:03 +0530

    eslitn added

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+	env: {
+		commonjs: true,
+		es6: true,
+		node: true,
+		browser: true
+	},
+	extends: ["eslint:recommended"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parserOptions: {
+		ecmaVersion: 2018
+	},
+	rules: {
+		strict: "error"
+	}
+};
