7af7b880cec864e36d105931320995160d7aa140
Sparkenstein <zetabytes.pp@gmail.com>
2020-02-12 15:37:06 +0530

    created package script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Imports
 const express = require("express");
 const fileUploder = require("./core/fileUploader");
+const { existsSync, mkdirSync } = require("fs");
 
 // constants
 const PORT = parseInt(process.env.PORT, 10) || 4000;
@@ -20,5 +21,9 @@ app.post("/upload", (req, res) => {
 });
 
 app.listen(PORT, () => {
+	// Using synchronous variants deliberately
+	if (!existsSync("uploads")) {
+		mkdirSync("uploads");
+	}
 	console.log(`Server started at http://localhost:${PORT}`);
 });
